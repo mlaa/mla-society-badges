396bd427f69ce77ed74362066e066a5f77c60c5d
Ryan Williams <ryrowi@gmail.com>
2016-07-06 17:58:21 -0400

    show badges on profile & hide in widgets

--- mla-society-badges.php
+++ mla-society-badges.php
@@ -8,17 +8,34 @@ namespace MLA\Commons\Plugin\SocietyBadges;
 
 function add_badges( $img ) {
 	$badges = '';
-	$member_types = \bp_get_member_type( \bp_get_member_user_id(), false );
 
-	foreach ( $member_types as $type ) {
-		$badges .= "<span class=\"society-badge $type\"></span>";
+	/**
+	 * member profile requires bp_get_displayed_user*()
+	 * member directory requires bp_get_member*()
+	 */
+	if ( $user = \bp_get_displayed_user() ) {
+		$user_id = $user->id;
+	} else {
+		$user_id = \bp_get_member_user_id();
+	}
+
+	if ( $member_types = \bp_get_member_type( $user_id, false ) ) {
+		foreach ( $member_types as $type ) {
+			$badges .= "<span class=\"society-badge $type\"></span>";
+		}
 	}
 
 	return $badges . $img;
 }
-\add_filter( 'bp_member_avatar', __NAMESPACE__ . '\\add_badges' );
 
 function enqueue_style() {
 	\wp_enqueue_style( 'mla_society_badges', \plugins_url() . '/mla-society-badges/css/style.css' );
 }
-\add_action( 'wp_enqueue_scripts',  __NAMESPACE__ . '\\enqueue_style' );
+
+function init() {
+	if ( \bp_is_members_directory() || \bp_is_user_profile() ) {
+		\add_filter( 'bp_member_avatar', __NAMESPACE__ . '\\add_badges' );
+		\add_action( 'wp_enqueue_scripts',  __NAMESPACE__ . '\\enqueue_style' );
+	}
+}
+\add_filter( 'bp_init', __NAMESPACE__ . '\\init' );
