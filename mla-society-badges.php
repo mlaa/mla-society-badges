a3e10554cd42efa272dc20e6621ca517a413bdc4
Ryan Williams <ryrowi@gmail.com>
2016-07-13 13:41:50 -0400

    add group avatars on directory & single views

--- mla-society-badges.php
+++ mla-society-badges.php
@@ -6,9 +6,7 @@
 
 namespace MLA\Commons\Plugin\SocietyBadges;
 
-function add_badges( $img ) {
-	$badges = '';
-
+function add_member_badges( $img ) {
 	/**
 	 * member profile requires bp_get_displayed_user*()
 	 * member directory requires bp_get_member*()
@@ -19,8 +17,24 @@ function add_badges( $img ) {
 		$user_id = \bp_get_member_user_id();
 	}
 
-	if ( $member_types = \bp_get_member_type( $user_id, false ) ) {
-		foreach ( $member_types as $type ) {
+	return add_badges( \bp_get_member_type( $user_id, false ), $img );
+}
+
+function add_group_badges( $img ) {
+	// group_id for directory, current_group_id for single
+	$group_id = \bp_get_group_id() || \bp_get_current_group_id();
+
+	return add_badges( \bp_groups_get_group_type( $group_id, false ), $img );
+}
+
+/**
+ * helper function used in member, group, & site contexts
+ */
+function add_badges( $types, $img ) {
+	$badges = '';
+
+	if ( $types ) {
+		foreach ( $types as $type ) {
 			$badges .= "<span class=\"society-badge $type\"></span>";
 		}
 	}
@@ -33,9 +47,23 @@ function enqueue_style() {
 }
 
 function init() {
+
 	if ( \bp_is_members_directory() || \bp_is_user_profile() ) {
-		\add_filter( 'bp_member_avatar', __NAMESPACE__ . '\\add_badges' );
+
+		\add_filter( 'bp_member_avatar', __NAMESPACE__ . '\\add_member_badges' );
 		\add_action( 'wp_enqueue_scripts',  __NAMESPACE__ . '\\enqueue_style' );
+
+	} else if ( \bp_is_groups_directory() || \bp_is_group() ) {
+
+		\add_filter( 'bp_get_group_avatar', __NAMESPACE__ . '\\add_group_badges' );
+		\add_action( 'wp_enqueue_scripts',  __NAMESPACE__ . '\\enqueue_style' );
+
+//	} else if ( \bp_is_sites_directory() ) {
+//
+//		\add_filter( 'bp_site_avatar', __NAMESPACE__ . '\\add_site_badges' );
+//		\add_action( 'wp_enqueue_scripts',  __NAMESPACE__ . '\\enqueue_style' );
+//
 	}
+
 }
 \add_filter( 'bp_init', __NAMESPACE__ . '\\init' );
